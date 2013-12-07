6981086a3cba7eb22ef6d4c85d6e35da80307db8
NivashiniNuappz <nivashini@nuappz.com>
2013-12-07 16:39:04 +0530

    Sample
    I made changes only to the id for the input type in
    fertilizer_master.html.

--- app/js/app.js
+++ app/js/app.js
@@ -3,12 +3,8 @@
 /* App Module */
 
 var phonecatApp = angular.module('phonecatApp', [
-  'ngRoute',
-  'phonecatAnimations',
-
-  'phonecatControllers',
-  'phonecatFilters',
-  'phonecatServices'
+  'ngRoute'
+ 
 ]);
 
 phonecatApp.config(['$routeProvider',
